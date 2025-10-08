e631c2aac0d3ccf7f3dff24d9d32f507a0f4a9f7
Gustavo Boschini <gustavoboschini.r@gmail.com>
2025-10-08 18:35:40 -0300

    MUITA COISA REMOVIDA DE BANCO E AUTH, VE SE FIZ MERDA

--- run.bat
+++ run.bat
@@ -3,12 +3,6 @@
 REM Muda para o diretório onde o script está
 cd /d "%~dp0"
 
-REM Lê o arquivo .env e define as variáveis de ambiente
-FOR /F "tokens=1,* delims==" %%A IN (.env) DO (
-    echo %%A=%%B
-    set %%A=%%B
-)
-
 REM Define o profile de desenvolvimento
 set SPRING_PROFILES_ACTIVE=dev
 
